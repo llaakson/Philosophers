4b0c97e94b4fe660b8cb791331e33da92cce2719
Leo Laaksonen <leo.laaksonenu@hotmail.com>
2025-02-09 08:06:12 +0200

    sleep,eat can't split the forks

--- main.c
+++ main.c
@@ -1,44 +1,69 @@
 #include "philo.h"
 
-void	*philosopher(void *ms)
+pthread_mutex_t mutex;
+
+void	philo_think(t_ms *ms)
 {
-	struct s_ms *msp = (struct s_ms*)ms;
-	usleep(msp->time_to_die);
 	size_t time;
-	printf("Time to die %ld \n", time = get_time());
+
+	time = get_time() - ms->start;
+	printf("%ld %d is thinking\n", time, ms->name);
+	//usleep(ms->time_to_think * 1000);
+}
+
+void	philo_sleep(t_ms *ms)
+{
+	size_t time;
+
+	time = get_time() - ms->start;
+	printf("%ld %d is sleeping\n", time, ms->name);
+	usleep(ms->time_to_sleep * 1000);
+}
+
+void philo_eat(t_ms *ms)
+{
+	size_t time;
+	
+	pthread_mutex_lock(ms->fork_left);
+	pthread_mutex_lock(ms->fork_right);
+	time = get_time() - ms->start;
+	printf("%ld %d is eating\n", time, ms->name);
+	usleep(ms->time_to_eat * 1000);
+	pthread_mutex_unlock(ms->fork_left);
+	pthread_mutex_lock(ms->fork_right);
+}
+
+void	*philosopher(void *ptr)
+{
+	t_ms *ms;
+
+	ms = (t_ms *)ptr;
+	//printf("Created thread name %d\n", ms->name);
+	while (ms->number_of_meals != 0)
+	{
+		philo_eat(ms);
+		philo_sleep(ms);
+		//philo_think(ms);
+		ms->number_of_meals -= 1;
+	}
 	return (NULL);
 }
 
-int create_threads(t_ms *ms)
+int create_threads(t_ms *ms, t_philosopher *pp)
 {
-	pthread_t philo[200];
 	int i;
 
 	i = 0;
-	while (i < ms->number_of_philosophers) 
+	while (i < pp->philos[0].number_of_philosophers) 
 	{
-		pthread_create(philo + i, NULL, &philosopher, (void *) ms);
+		pthread_create(&pp->philos[i].thread, NULL, &philosopher, &pp->philos[i]);
+		//printf("%d is threading\n", pp->philos[i].name);
 		i++;
 	}
 	i = 0;
 	while (i < ms->number_of_philosophers)
 	{
-		pthread_join(philo[i], NULL);
-		i++;
-	}
-	return (0);
-}
-
-int init_philosophers(t_ms *ms, pthread_mutex_t *forks)
-{	
-	int i;
-
-	i = 0;
-	while (i < ms->number_of_philosophers)
-	{	
-		ms->name = i;
-		ms->fork_left = forks[i];
-		ms->fork_right = forks[i];
+		pthread_join(pp->philos[i].thread, NULL);
 		i++;
 	}
 	return (0);
@@ -50,38 +75,60 @@ int init_philo(t_ms *ms, char **argv)
 	ms->time_to_die = ft_atoi(argv[2]);
 	ms->time_to_eat = ft_atoi(argv[3]);
 	ms->time_to_sleep = ft_atoi(argv[4]);
-	printf("Time to die: %d\n", ms->time_to_die);
+	ms->number_of_meals = ft_atoi(argv[5]);
+	printf("Init_philo\n");
 	return (ms->time_to_die);
 }
 
-int init_forks(pthread_mutex_t *forks, t_ms *ms)
+int init_philosophers(t_ms *ms, pthread_mutex_t *forks, char **argv)
+{	
+	int i;
+
+	i = 0;
+	while (i < ft_atoi(argv[1]))
+	{	
+		ms[i].name = i + 1;
+		if (i == 0)
+			ms[i].fork_left = &forks[ft_atoi(argv[1])];
+		else
+			ms[i].fork_left = &forks[i - 1];
+		ms[i].fork_right = &forks[i];
+		init_philo(&ms[i], argv);
+		ms[i].start = get_time();
+		i++;
+	}
+	return (0);
+}
+
+int init_forks(pthread_mutex_t *forks, char **argv)
 {
 	int	i;
 
 	i = 0;
-	while (i < ms->number_of_philosophers)
+	while (i < ft_atoi(argv[1]))
 	{
 		pthread_mutex_init(&forks[i], NULL);
 		i++;
 	}
+	pthread_mutex_init(&mutex, NULL);
 	return (0);
 	
 }
 int main(int argc, char **argv)
 {
-	t_ms ms;
-	//t_philosopher philo;
+	t_ms ms[200];
+	t_philosopher philo;
+	philo.philos = ms;
 	pthread_mutex_t forks[200];
 
-	if (argc < 5 || argc >= 6)
+	if (argc <= 4 || argc >= 7)
 	{
 		write(2, "Error ac!\n", 10);
 		return (1);
 	}
-	init_philo(&ms, argv);
-	init_forks(forks, &ms);
-	init_philosophers(&ms, forks);
-	create_threads(&ms);
+	init_forks(forks, argv);
+	init_philosophers(ms, forks, argv);
+	create_threads(ms, &philo);
 	printf("Works\n");
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -5,21 +5,23 @@
 # include <unistd.h>
 # include <sys/time.h>
 
-typedef struct s_philosopher {
-	int	name;
-} t_philosopher;
-
 typedef struct s_ms {
 	int	number_of_philosophers;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	number_of_must_eat;
+	int	number_of_meals;
 	int	name;
-	pthread_mutex_t	fork_left;
-	pthread_mutex_t	fork_right;
-	t_philosopher	*philosophers;
+	size_t	start;
+	pthread_t thread;
+	pthread_mutex_t	*fork_left;
+	pthread_mutex_t	*fork_right;
 } t_ms;
+
+typedef struct s_philosopher {
+	t_ms	*philos;
+} t_philosopher;
 
 int     ft_atoi(const char *str);
 size_t  get_time();
